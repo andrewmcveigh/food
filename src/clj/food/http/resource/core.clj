d098846a80925b169058379e02e03f21a76b4ff2
Andrew Mcveigh <me@andrewmcveigh.com>
2014-10-29 18:31:10 +0100

    Add default resource/handle-not-found

--- src/clj/food/http/resource/core.clj
+++ src/clj/food/http/resource/core.clj
@@ -3,7 +3,11 @@
    [liberator.core :refer [defresource]]))
 
 (def defaults
-  {:available-media-types #{"text/html"}})
+  {:available-media-types #{"text/html"}
+   :handle-not-found (fn [_] "Not found")})
 
 (defresource root defaults
   :handle-ok (fn [_] "Hello World!"))
+
+(defresource default defaults
+  :exists? false)
